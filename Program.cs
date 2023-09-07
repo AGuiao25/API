d3f853259dca67db663664e0dc09dc7b47155d67
DBASYSTEM\Angel.Guiao <AngelGuiao>
2023-09-07 09:59:49 +0800

    Adding authentication, json web tokens

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
 
-builder.Services.AddCors();
 
 var app = builder.Build();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
-app.MapControllers();
 
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.UseAuthentication();
 app.Run();
