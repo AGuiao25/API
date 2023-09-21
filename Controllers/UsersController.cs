bfdf43422581c938e534425f6e5a8a34e9c3e81d
DBASYSTEM\Angel.Guiao <AngelGuiao>
2023-09-21 14:42:08 +0800

    Server update user

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -20,14 +21,14 @@ namespace API.Controllers
             _mapper = mapper;
         }
 
-     [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-        {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+            {
 
-            var users = await _userRepository.GetMembersAsync();
+                var users = await _userRepository.GetMembersAsync();
 
-            return Ok(users);
-        }
+                return Ok(users);
+            }
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
@@ -37,5 +38,23 @@ namespace API.Controllers
            return await _userRepository.GetMemberAsync(username);
         }
 
+        
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
+
     }
+
+
 }
